584ae71f592243a0b7e9ea884fba999c6e32bbab
Al-un <alun.sng@gmail.com>
2023-02-08 23:33:47 +0900

    add user model

---
+++ ressaite-api/src/um/models/user.ts
@@ -0,0 +1,32 @@
+import { sequelize } from "../../core/db/instance";
+import {
+  CreationOptional,
+  DataTypes,
+  InferAttributes,
+  InferCreationAttributes,
+  Model,
+} from "sequelize";
+
+export class User extends Model<
+  InferAttributes<User>,
+  InferCreationAttributes<User>
+> {
+  declare id: CreationOptional<number>;
+  declare username: string;
+  declare password: string;
+  declare email: string | null;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: { type: new DataTypes.STRING(128), allowNull: false },
+    password: { type: new DataTypes.STRING(128), allowNull: false },
+    email: { type: new DataTypes.STRING(128) },
+  },
+  { tableName: "users", sequelize }
+);
